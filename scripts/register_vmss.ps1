03029e851ea3bddd36aaa146342ee2c1c48b3830
Liam Smith <liam.smith@microsoft.com>
2024-01-02 19:12:29 +0000

    Push

--- scripts/register_vmss.ps1
+++ scripts/register_vmss.ps1
@@ -17,10 +17,10 @@ Write-Verbose "Service Principal Name: $vmss_operator_name" -Verbose
 Write-Verbose "Subscription Name: $subscriptionName" -Verbose
 
 $vmss_sp = az ad app list --output json --query "[?displayName=='$($vmss_operator_name)']" | ConvertFrom-Json -Depth 10
-if ($vmss_sp -eq $null) {
-  $vmss_sp = az ad sp create-for-rbac --name $vmss_operator_name --role "Virtual Machine Contributor" --scopes $ado_agent_pool_vmss_id --json-auth --output json | convertfrom-json -Depth 10
+if ($null -eq $vmss_sp) {
+  $vmss_sp = az ad sp create-for-rbac --name $vmss_operator_name --role "Virtual Machine Contributor" --scopes $ado_agent_pool_vmss_id --json-auth --output json | ConvertFrom-Json -Depth 10
 } else {
-  if($spObject.Length -gt 1){
+  if($vmss_sp.Length -gt 1){
     Write-Error "Found too many Service Principals"
     throw "Found too many Service Principals"
   }
@@ -31,35 +31,35 @@ if ($vmss_sp -eq $null) {
     foreach($key in $keys){
         Write-Verbose ("Removing key with displayName {0}" -f $key.name)
         $payload = @{ keyId = $key.id } | ConvertTo-Json -Depth 10 | Out-File -FilePath payload.json -Encoding ascii -Force
-        az rest --method post --url https://graph.microsoft.com/v1.0/applications/$($spObject.id)/removePassword --body $payload --headers $headers
+        az rest --method post --url https://graph.microsoft.com/v1.0/applications/$($vmss_sp.id)/removePassword --body $payload --headers $headers
     }
   
   Write-Verbose ("Creating key with displayName" -f $sp.name)
   $payload = @{ passwordCredential = @{ displayName = "rbac" }} | ConvertTo-Json -Depth 10
-  $headers = @{ "Content-Type" = "application/json" } | ConvertTo-Json -Depth 10
-  $output = az rest --method post --url https://graph.microsoft.com/v1.0/applications/$($spObject.id)/addPassword --body $payload --headers $headers
-  $outputObj = $output | ConvertFrom-Json -depth 10
+  $payload | Out-File -FilePath "payload.json" -Encoding ascii -Force
+  $output = az rest --method post --url https://graph.microsoft.com/v1.0/applications/$($vmss_sp.id)/addPassword --body "@payload.json"
+  $vmss_sp_object = $output | ConvertFrom-Json -depth 10
   $vmss_sp = @{
-    clientId = $spObject.appId
-    clientSecret = $outputObj.secretText
+    clientId = $vmss_sp.appId
+    clientSecret = $vmss_sp_object.secretText
     subscriptionId = $ado_agent_pool_vmss_id.Split("/")[2]
-    displayName = $spName
+    displayName = $vmss_operator_name
     tenantId = $(az account show --query "tenantId")
   }
 }
 
-if( "${{ parameters.ADOServiceConnection }}" -ne "" ){
+if( $ado_service_connection_name -ne "" ){
   Write-Verbose "Logging out of Service Connection"
   az logout
-  Write-Verbose "Logging into Service Connection ${{ parameters.ADOServiceConnection }}"
-  az login --service-principal --username "$(ADO_CLIENT_ID)" --password "$(ADO_CLIENT_SECRET)" --tenant "$(ADO_TENANT_ID)"
+  Write-Verbose "Logging into Service Connection $ado_service_connection_name"
+  az login --service-principal --username $env:ADO_CLIENT_ID --password $env:ADO_CLIENT_SECRET --tenant $env:ADO_TENANT_ID
 }
 
-$accessToken = az account get-access-token --resource '499b84ac-1321-427f-aa17-267ca6975798' --query 'accessToken' --output tsv
+# $accessToken = az account get-access-token --resource '499b84ac-1321-427f-aa17-267ca6975798' --query 'accessToken' --output tsv
 
-$uri = "https://dev.azure.com/$ado_org/_apis/projects?api-version=7.1-preview.4"
-Write-Verbose "Trying for projects: $uri" -Verbose
-$ado_projects = az rest --uri $uri --method get --resource '499b84ac-1321-427f-aa17-267ca6975798' --output json
+$projects_uri = "https://dev.azure.com/$ado_org/_apis/projects?api-version=7.1-preview.4"
+Write-Verbose "Trying for projects: $projects_uri" -Verbose
+$ado_projects = az rest --uri $projects_uri --method get --resource '499b84ac-1321-427f-aa17-267ca6975798' --output json
 Write-Verbose ("Projects: {0}" -f ($ado_projects | ConvertFrom-Json -Depth 10 | ConvertTo-Json -Compress)) -Verbose
 $ado_projectsObject = $ado_projects | ConvertFrom-Json -Depth 10
 $ado_projectTargetObject = $ado_projectsObject.value | where-object { $_.name -eq $ado_project }
@@ -67,26 +67,27 @@ $ado_projectTargetObject = $ado_projectsObject.value | where-object { $_.name -e
 if ($ado_projectTargetObject -eq $null) {
   Write-Error "Project Not Found"
 }
-$ado_projectId = $ado_projectTargetObject.id
+$ado_project_id = $ado_projectTargetObject.id
 
 # Search for service connection
-$uri = "https://dev.azure.com/$ado_org/$ado_project/_apis/serviceendpoint/endpoints?api-version=7.1-preview.4"
-Write-Verbose "Trying for service connections: $uri" -Verbose
-$serviceEndpoints = az rest --uri $uri --method get --resource '499b84ac-1321-427f-aa17-267ca6975798' --output json
-Write-Verbose ("Service Connections: {0}" -f ($serviceEndpoints | ConvertFrom-Json -Depth 10 | ConvertTo-Json -Compress)) -Verbose
-$serviceEndpointsSearch = ($serviceEndpoints | ConvertFrom-Json -Depth 10)
+$service_connections_uri = "https://dev.azure.com/$ado_org/$ado_project/_apis/serviceendpoint/endpoints?api-version=7.1-preview.4"
+Write-Verbose "Trying for service connections: $service_connections_uri" -Verbose
+$service_endpoints = az rest --uri $service_connections_uri --method get --resource '499b84ac-1321-427f-aa17-267ca6975798' --output json
+Write-Verbose ("Service Connections: {0}" -f ($service_endpoints | ConvertFrom-Json -Depth 10 | ConvertTo-Json -Compress)) -Verbose
+$service_endpoints_search = ($service_endpoints | ConvertFrom-Json -Depth 10)
 
-if ($serviceEndpointsSearch -ne $null) {
-  if (($serviceEndpointsSearch.value | where-object { $_.name -eq $ado_service_connection_name }) -ne $null) {
+if ($null -ne $service_endpoints_search) {
+  if ($null -ne ($service_endpoints_search.value | where-object { $_.name -eq $ado_service_connection_name })) {
     Write-Verbose "Service Connection Already Exists" -Verbose
-    $serviceEndpointObject = $serviceEndpointsSearch.value | where-object { $_.name -eq $ado_service_connection_name }
+    $service_endpoints_object = $service_endpoints_search.value | where-object { $_.name -eq $ado_service_connection_name }
   }
+  # Write logic where Service Endpoint exists and we need to push the new secret
 }
 
-if ($serviceEndpointObject -eq $null) {
+if ($null -eq $service_endpoints_object) {
   $payload = @{
     data = @{
-      subscriptionId = "$($spObject.subscriptionId)"
+      subscriptionId = "$($vmss_sp.subscriptionId)"
       subscriptionName = "$subscriptionName"
       environment = "AzureCloud"
       scopeLevel = "Subscription"
@@ -97,10 +98,10 @@ if ($serviceEndpointObject -eq $null) {
     url = "https://management.azure.com/"
     authorization = @{
       parameters = @{
-        tenantid = $spObject.tenantId
-        serviceprincipalid = $spObject.clientId
+        tenantid = $vmss_sp.tenantId
+        serviceprincipalid = $vmss_sp.clientId
         authenticationType = "spnKey"
-        serviceprincipalkey = $spObject.clientSecret
+        serviceprincipalkey = $vmss_sp.clientSecret
       }
       scheme = "ServicePrincipal"
     }
@@ -109,7 +110,7 @@ if ($serviceEndpointObject -eq $null) {
     serviceEndpointProjectReferences = @(
       @{
         projectReference = @{
-          id = $ado_projectId
+          id = $ado_project_id
           name = $ado_project
         }
         name = $ado_service_connection_name
@@ -117,31 +118,32 @@ if ($serviceEndpointObject -eq $null) {
     )
   } | ConvertTo-Json -Compress -Depth 10
   $payload | Out-File -FilePath payload.json -Encoding ascii -Force
-  Write-Verbose ("Payload for Service Endpoint: {0}" -f ($payload.replace($spObject.clientSecret, "*"))) -Verbose
+  Write-Verbose ("Payload for Service Endpoint: {0}" -f ($payload.replace($vmss_sp.clientSecret, "*"))) -Verbose
   
-  $uri = "https://dev.azure.com/$ado_org/_apis/serviceendpoint/endpoints?api-version=7.1-preview.4"
-  Write-Verbose ("Trying to create service endpoint: {0}" -f $uri) -Verbose
+  $service_connection_uri = "https://dev.azure.com/$ado_org/_apis/serviceendpoint/endpoints?api-version=7.1-preview.4"
+  Write-Verbose ("Trying to create service endpoint: {0}" -f $service_connection_uri) -Verbose
   
-  $serviceEndpointResponse = az rest --uri $uri --method post --resource '499b84ac-1321-427f-aa17-267ca6975798' --output json --body '@payload.json'
-  $serviceEndpointObject = $serviceEndpointResponse | ConvertFrom-Json -Depth 10
+  $service_endpoints_response = az rest --uri $service_connection_uri --method post --resource "499b84ac-1321-427f-aa17-267ca6975798" --output json --body "@payload.json"
+  $service_endpoints_object = $service_endpoints_response | ConvertFrom-Json -Depth 10
 }
 
-$ElasticPoolsUrl = "https://dev.azure.com/$ado_org/_apis/distributedtask/elasticpools?api-version=7.0"
-Write-Verbose ("Trying for elastic pools: {0}" -f $ElasticPoolsUrl) -Verbose
-$ElasticPools = az rest --uri $ElasticPoolsUrl --method get --resource '499b84ac-1321-427f-aa17-267ca6975798' --output json
-Write-Verbose ("Elastic Pools: {0}" -f ($ElasticPools | ConvertFrom-Json -Depth 10 | ConvertTo-Json -Compress)) -Verbose
-$ElasticPoolsObject = $ElasticPools | ConvertFrom-Json -Depth 10
+$elastic_pools_uri = "https://dev.azure.com/$ado_org/_apis/distributedtask/elasticpools?api-version=7.0"
+Write-Verbose ("Trying for elastic pools: {0}" -f $elastic_pools_uri) -Verbose
+$elastic_pools = az rest --uri $elastic_pool_uri --method get --resource '499b84ac-1321-427f-aa17-267ca6975798' --output json
+Write-Verbose ("Elastic Pools: {0}" -f ($elastic_pools | ConvertFrom-Json -Depth 10 | ConvertTo-Json -Compress)) -Verbose
+$elastic_pools_object = $elastic_pools | ConvertFrom-Json -Depth 10
 
 # Check if the VMSS is already registered
-if (($ElasticPoolsObject.value | Where-Object { $_.azureId -eq $ado_agent_pool_vmss_id }) -eq $null) {
+if ($null -eq ($elastic_pools_object.value | Where-Object { $_.azureId -eq $ado_agent_pool_vmss_id })) {
   Write-Verbose "VMSS is not registered"
   Write-Verbose "Sleep for 30; let Azure DevOps Service Connection be ready" -Verbose
   Start-Sleep -Seconds 30
-  $poolname = $ado_service_connection_name
+  $pool_name = $ado_service_connection_name
   $authorizeAllPipelines = $false
   $autoProvisionProjectPools = $false
-  $uri = "https://dev.azure.com/$ado_org/_apis/distributedtask/elasticpools?poolName={0}&authorizeAllPipelines={1}&autoProvisionProjectPools={2}&projectId={3}&api-version=7.0" -f $poolname, $authorizeAllPipelines, $autoProvisionProjectPools, $ado_projectId
-  Write-Verbose ("Trying to register VMSS: {0}" -f $uri) -Verbose
+  $agent_pool_uri = "https://dev.azure.com/$ado_org/_apis/distributedtask/elasticpools?poolName={0}&authorizeAllPipelines={1}&autoProvisionProjectPools={2}&projectId={3}&api-version=7.0"
+  $agent_pool_uri = $agent_pool_uri -f $pool_name, $authorizeAllPipelines, $autoProvisionProjectPools, $ado_project_id
+  Write-Verbose ("Trying to register VMSS: {0}" -f $agent_pool_uri) -Verbose
   $payload = @{
     agentInteractiveUI = $false
     azureId = $ado_agent_pool_vmss_id
@@ -150,14 +152,14 @@ if (($ElasticPoolsObject.value | Where-Object { $_.azureId -eq $ado_agent_pool_v
     maxSavedNodeCount = 0
     osType = 1
     recycleAfterEachUse = $true
-    serviceEndpointId    = $($serviceEndpointObject.id)
-    serviceEndpointScope = $($serviceEndpointObject.serviceEndpointProjectReferences[0].projectReference.id)
+    serviceEndpointId    = $($service_endpoints_object.id)
+    serviceEndpointScope = $ado_project_id
     timeToLiveMinutes    = 15
   }
-  $payloadJson = $payload | ConvertTo-Json -Compress -Depth 10
-  Write-Verbose ("Payload: {0}" -f ($payloadJson)) -Verbose
-  Out-File -FilePath payload.json -Encoding ascii -Force -InputObject $payloadJson
-  $NewPool = az rest --uri $uri --method post --resource '499b84ac-1321-427f-aa17-267ca6975798' --output json --body '@payload.json'
+  $payload_json = $payload | ConvertTo-Json -Compress -Depth 10
+  Write-Verbose ("Payload: {0}" -f ($payload_json)) -Verbose
+  Out-File -FilePath payload.json -Encoding ascii -Force -InputObject $payload_json
+  az rest --uri $uri --method post --resource '499b84ac-1321-427f-aa17-267ca6975798' --output json --body '@payload.json'
   Write-Verbose "Successfully Register VMSS" -Verbose
 } else {
   Write-Verbose "Failed to Register VMSS - VMSS is already registered" -Verbose
