52be87a9a3e405407ac269088dce395613e0193c
System Administrator <admin@MngEnvMCAP174659.onmicrosoft.com>
2024-01-14 12:30:30 +0000

    Added plan.ps1

---
+++ scripts/terraform/plan.ps1
@@ -0,0 +1,40 @@
+param(
+    [string]$refresh_plan = $false
+    [string]$terraform_vars, # JSON string NEED TO ADD SUPPORT
+    [string]$terraform_var_file,
+    [string]$terraform_plan_file = 'terraform.plan'
+)
+
+$terraform_params = @()
+$terraform_params += '-input=false' # Don't prompt for input
+
+if($terraform_plan_file){
+    $terraform_params += "-out=$terraform_plan_file"
+}
+
+if($refresh_plan){
+    $terraform_params += '-refresh=true'
+}
+
+if($terraform_var_file){
+    $terraform_params += "-var-file=$terraform_var_file"
+}
+
+if($terraform_vars){
+    $terraform_params += "-detailed-exitcode"
+}
+
+Write-Verbose ("Running 'terraform plan {0}'" -f ($terraform_params -join ' ')) -Verbose
+terraform plan @terraform_params
+
+$terraform_exit_code = $LASTEXITCODE
+
+if($terraform_exit_code -eq 0){
+    Write-Verbose "Terraform plan succeeded with no changes required" -Verbose
+} elseif($terraform_exit_code -eq 1){
+    Write-Verbose "Terraform plan failed" -Verbose
+} elseif($terraform_exit_code -eq 2){
+    Write-Verbose "Terraform plan succeeded with changes required" -Verbose
+} else {
+    Write-Verbose "Terraform plan returned an unknown exit code: $terraform_exit_code" -Verbose
+}
