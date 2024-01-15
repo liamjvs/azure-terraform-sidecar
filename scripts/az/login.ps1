1bd21e31453b1bbfacf256d87185f8feac12ef24
System Administrator <admin@MngEnvMCAP174659.onmicrosoft.com>
2024-01-15 11:21:56 +0000

    Updated login.ps1

--- scripts/az/login.ps1
+++ scripts/az/login.ps1
@@ -10,10 +10,8 @@ if($managed_identity) {
     az login --identity
     $env:ARM_TENANT_ID = (az account show --query tenantId -o tsv)
     $env:ARM_USE_MSI = "true"
-} elseif ($env:ARM_CLIENT_ID -and $env:ARM_CLIENT_SECRET -and $env:ARM_TENANT_ID) {
+} else {
     # The AzureCLI task performs a `az account clear` at the end. We want a persistent login across tasks.
     Write-Verbose "Using Service Principal" -Verbose
     az login --service-principal --username $env:ARM_CLIENT_ID --password $env:ARM_CLIENT_SECRET --tenant $env:ARM_TENANT_ID
-} else {
-    Write-Error "Login failed; no credentials in environment variables"
 }
