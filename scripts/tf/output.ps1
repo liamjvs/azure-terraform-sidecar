96450cfc16886e41fe0bdef18b3c0d66abbca393
Liam Smith <liam.smith@microsoft.com>
2024-01-21 21:31:18 +0000

    Go

--- scripts/tf/output.ps1
+++ scripts/tf/output.ps1
@@ -19,7 +19,7 @@ if($output_object -eq $null){
 foreach($key in $output_object.PSObject.Properties.Name){
     $line_item = "{0} = `"{1}`"" -f $key, $output_object.$key.value
     if($cicd_ado){
-        Write-Host ("##vso[task.setvariable variable=TF_OUTPUT_{0};]{1};isOutput=true;" -f ($key.ToUpper()), $output_object.$key.value)
+        Write-Host ("##vso[task.setvariable variable=TF_OUTPUT_{0};isOutput=true;]{1}" -f ($key.ToUpper()), $output_object.$key.value)
     }
     Write-Verbose ("TF_OUTPUT_{0} set to {1}" -f $key.ToUpper(), $output_object.$key.value) -Verbose
 }
