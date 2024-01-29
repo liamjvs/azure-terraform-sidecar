152c6fa2239902d7b98ee67b84221cd2a7c89975
Liam Smith <liam.smith@microsoft.com>
2024-01-29 22:55:59 +0000

    Go

--- scripts/utils/check_state_file.ps1
+++ scripts/utils/check_state_file.ps1
@@ -5,7 +5,7 @@ param(
 )
 
 Write-Verbose ("Checking for {0} file in {1}" -f $terraform_state_file,$terraform_working_dir) -Verbose
-if(Test-Path -Path "$terraform_working_dir/$terraform_state_file"){
+if(Test-Path -Path "$terraform_working_dir/$terraform_state_file" -ErrorAction SilentlyContinue){
     if($fail_on_existance){
         Write-Error "Found $terraform_state_file in $terraform_working_dir. Please remove this file before continuing."
         exit 1
