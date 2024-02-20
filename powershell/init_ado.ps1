ab2e30070e49229a3a5bab4c73ecdbb24e3fac4a
Liam Smith <liam.smith@microsoft.com>
2024-02-20 23:20:50 +0000

    Go

--- powershell/init_ado.ps1
+++ powershell/init_ado.ps1
@@ -3,15 +3,17 @@
 param(
     [string]$ado_organization = "https://dev.azure.com/lismithmcaps",
     [string]$ado_project = "Code",
+    [string]$repo_name,
     [string]$service_connection_name,
     [string]$azure_subscription_id = "3f96fd38-eec2-48f5-8d76-c9b6c17c8c95",
     [string]$service_principal_id
 )
 
 try {
-    Import-Module ./scripts/ado/funcs/agent_pool.ps1
-    Import-Module ./scripts/ado/funcs/service_connection.ps1
-    Import-Module ./scripts/ado/funcs/project.ps1
+    Import-Module ./scripts/ado/funcs/agent_pool.ps1 -Force
+    Import-Module ./scripts/ado/funcs/service_connection.ps1 -Force
+    Import-Module ./scripts/ado/funcs/project.ps1 -Force
+    Import-Module ./scripts/ado/funcs/repo.ps1 -Force
 }
 catch {
     Write-Error "Failed to import the required modules. Please make sure the modules are present in the correct location."
@@ -67,6 +69,18 @@ if(!$ado_projectObject) {
 }
 $ado_project_id = $ado_projectObject.id
 
+# prompt user for their repo name
+if(!$repo_name) {
+    $ado_repositories = Get-Repositories -ado_org $ado_organization -project_id $ado_project_id
+    while(!$repo_name){
+        $answer = Read-Host "Enter the repo name you want to use (must be valid)"
+        if($ado_repositories -notcontains $answer) {
+            Write-Host "Repo name must be valid"
+        } else {
+            $repo_name = $repo_name
+        }
+    }
+}
 
 # get the current azure subscription name and id into json
 $azure_subscription = az account show --output json
@@ -109,13 +123,26 @@ if(!$service_principal_id) {
 
 # prompt the user for their service connection name
 if(!$service_connection_name) {
-    $service_connection_name = Read-Host "Enter the service connection name (blank: sidecar-service-connection)"
-    if(!$service_connection_name) {
-        $service_connection_name = "sidecar-service-connection"
+    $service_connection_to_be = Read-Host "Enter the service connection name (blank: sidecar-service-connection)"
+    if(!$service_connection_to_be) {
+        $service_connection_to_be = "sidecar-service-connection"
     }
 }
 
-# check service connection name is unique
+# loop through the service connections to check if the service connection name already exists
+while($service_connection_to_be){
+    $service_connections = Get-ServiceConnections -ado_org $ado_organization -ado_project $ado_project
+    if($service_connections) {
+        $service_connection_names = $service_connections.value.name
+        if($service_connection_names -contains $service_connection_to_be) {
+            $service_connection_to_be = Read-Host "Service Connection name already exists. Please enter a unique name."
+        } else {
+            $service_connection_name = $service_connection_to_be
+            $service_connection_to_be = $false
+        }
+    }
+
+}
 $service_connections = Get-ServiceConnections -ado_org $ado_organization -ado_project $ado_project
 if($service_connections) {
     $service_connection_names = $service_connections.value.name
@@ -125,16 +152,51 @@ if($service_connections) {
     }
 }
 
+$assign_creator_role_service_connections = Read-Host "Do you want to assign the creator role for service connections to the service principal? (default: y) (y/n)"
+if($assign_creator_role_service_connections -ne "n" -and $assign_creator_role_service_connections -ne "y") {
+    Write-Host "Invalid input. Defaulting to 'y'"
+    $assign_creator_role_service_connections = $true
+} elseif($assign_creator_role_service_connections -eq "n") {
+    $assign_creator_role_service_connections = $false
+} else {
+    $assign_creator_role_service_connections = $true
+}
+
+$assign_creator_role_agent_pools = Read-Host "Do you want to assign the creator role for agent pools to the service principal? (default: y) (y/n)"
+if($assign_creator_role_agent_pools -ne "n" -and $assign_creator_role_agent_pools -ne "y") {
+    Write-Host "Invalid input. Defaulting to 'y'"
+    $assign_creator_role_agent_pools = $true
+} elseif($assign_creator_role_agent_pools -eq "n") {
+    $assign_creator_role_agent_pools = $false
+} else {
+    $assign_creator_role_agent_pools = $true
+}
+
+$assign_repo_permissions = Read-Host "Do you want to assign the required access for your service connection to the repo? (default: y) (y/n)"
+if($assign_repo_permissions -ne "n" -and $assign_repo_permissions -ne "y") {
+    Write-Host "Invalid input. Defaulting to 'y'"
+    $assign_repo_permissions = $true
+} elseif($assign_repo_permissions -eq "n") {
+    $assign_repo_permissions = $false
+} else {
+    $assign_repo_permissions = $true
+}
+
+
 # Write new line
 Write-Host ""
 
 # Confirm with the user the values entered
 Write-Host "ADO Organization: $ado_organization"
 Write-Host "ADO Project: $ado_project"
+Write-Host "Repo Name: $repo_name"
 Write-Host "Subscription ID: $azure_subscription_id"
 Write-Host "Subscription Name: $subscription_name"
 Write-Host "Service Principal ID: $service_principal_id"
 Write-Host "Service Connection Name: $service_connection_name"
+Write-Host "Assign Creator Role for Service Connections: $assign_creator_role_service_connections"
+Write-Host "Assign Creator Role for Agent Pools: $assign_creator_role_agent_pools"
+Write-Host "Assign Repo Permissions: $assign_repo_permissions"
 $confirm = Read-Host "Do you want to continue? (y/n)"
 if($confirm -ne "y") {
     Write-Host "Exiting..."
@@ -169,12 +231,32 @@ $service_principal_object_id = az ad sp show --id $($service_principal).appId --
 Write-Host "Adding Service Principal to the ADO environment"
 $service_principal_entitlement = New-ServicePrincipalEntitlement -ado_org $ado_organization -service_principal_object_id $service_principal_object_id -project_id $ado_project_id
 
-Write-Host "Assigning Creator Role for Service Connections to Service Principal"
-$service_connection = Set-ServiceConnectionSecurity -ado_org $ado_organization -user_id $($service_principal_entitlement).id -project_id $ado_project_id -role "Creator"
+if($assign_creator_role_service_connections) {
+    Write-Host "Assigning Creator Role for Service Connections to Service Principal"
+    $service_connection = Set-ServiceConnectionSecurity -ado_org $ado_organization -user_id $($service_principal_entitlement).id -project_id $ado_project_id -role "Creator"
+}
 
-Write-Host "Assigning Creator Role for Agent Pools to Service Principal"
-$agent_pool = Set-AgentPoolSecurity -ado_org $ado_organization -project_id $ado_project_id -user_id $($service_principal_entitlement).id -role "Creator"
+if($assign_creator_role_agent_pools){
+    Write-Host "Assigning Creator Role for Agent Pools to Service Principal"
+    $agent_pool = Set-AgentPoolSecurity -ado_org $ado_organization -project_id $ado_project_id -user_id $($service_principal_entitlement).id -role "Creator"
+}
 
+if($assign_repo_permissions) {
+    Write-Host "Assigning required access to the repo"
+    if($build_service){
+        $descriptor = Get-BuildServiceSecurityID -ado_org $ado_organization -project_id $ado_project_id
+    } else {
+        $descriptor = "Microsoft.VisualStudio.Services.Claims.AadServicePrincipal;{0}\{1}" -f $($service_principal).tenant, $service_principal_object_id
+    }
+    $repo = Get-Repository -ado_org $ado_organization -project_id $ado_project_id -repo_name $repo_name
+
+    $roles = @("GenericContribute", "CreateBranch", "PullRequestContribute")
+    foreach ($role in $roles) {
+        $repo_security = Set-RepositorySecurity -ado_org $ado_organization -descriptor $descriptor -project_id $ado_project_id -repo_id $repo.id -role $role -allow $true
+        sleep 1
+        Write-Host ("Assigned $role to the {0} in $repo_name" -f ($build_service ? "build service" : "service principal"))
+    }
+}
 
 # Write new line
 Write-Host ""
---
+++ scripts/ado/funcs/repo.ps1
@@ -0,0 +1,111 @@
+function Get-Repositories {
+    param (
+        [Parameter(Mandatory=$true)][string]$ado_org,
+        [Parameter(Mandatory=$true)][string]$project_id
+    )
+
+    $uri = "$($ado_org)/$($project_id)/_apis/git/repositories?api-version=7.1-preview.1"
+    $out = az rest --uri $uri --method GET --resource "499b84ac-1321-427f-aa17-267ca6975798" --output json
+    $out = ($out | ConvertFrom-Json -Depth 10).value
+    return $out
+}
+
+function Get-Repository {
+    param (
+        [Parameter(Mandatory=$true)][string]$ado_org,
+        [Parameter(Mandatory=$true)][string]$project_id,
+        [Parameter(Mandatory=$true)][string]$repo_name
+    )
+
+    $out = Get-Repositories -ado_org $ado_org -project_id $project_id | Where-Object { $_.name -eq $repo_name }
+    if (!$out) {
+        Write-Error "Could not find the repository. This is unexpected."
+    }
+    return $out
+}
+
+function Get-BuildServiceSecurityID{
+    param (
+        [Parameter(Mandatory=$true)][string]$ado_org,
+        [Parameter(Mandatory=$true)][string]$project_id
+    )
+
+    $uri = "$($ado_org)/_apis/accesscontrollists/2e9eb7ed-3c0a-47d4-87c1-0ffdd275fd87"
+    $query = @{
+        "api-version" = "7.1-preview.1"
+        token = "repoV2/$($project_id)"
+    } | ConvertTo-Json -Compress | Out-File "query.json" -Force
+    $out = az rest --url $uri --method GET --resource "499b84ac-1321-427f-aa17-267ca6975798" --output json --uri-parameters "@query.json"
+    Remove-Item -Path "query.json" -Force
+    # force conver tot hashtable
+    $out = ($out | ConvertFrom-Json -Depth 10).value.acesDictionary.pSobject.Properties.Name | Where {$_ -like "Microsoft.TeamFoundation.ServiceIdentity*"}
+    if (!$out) {
+        Write-Error "Could not find the build service account. This is unexpected."
+    }
+    return $out
+}
+
+function Set-RepositorySecurity {
+    param (
+        [Parameter(Mandatory=$true)][string]$ado_org,
+        [Parameter(Mandatory=$true)][string]$descriptor,
+        [Parameter(Mandatory=$true)][string]$project_id,
+        [Parameter(Mandatory=$true)][string]$repo_id,
+        [Parameter(Mandatory=$true)][string]$role,
+        [Parameter(Mandatory=$true)][bool]$allow
+    )
+
+    $bitmap = @{
+        "Administer" = 1
+        "GenericRead" = 2
+        "GenericContribute" = 4
+        "ForcePush" = 8
+        "CreateBranch" = 16
+        "CreateTag" = 32
+        "ManageNote" = 64
+        "PolicyExempt" = 128
+        "CreateRepository" = 256
+        "DeleteRepository" = 512
+        "RenameRepository" = 1024
+        "EditPolicies" = 2048
+        "RemoveOthersLocks" = 4096
+        "ManagePermissions" = 8192
+        "PullRequestContribute" = 16384
+        "PullRequestBypassPolicy" = 32768
+        "ViewAdvSecAlerts" = 65536
+        "DismissAdvSecAlerts" = 131072
+        "ManageAdvSecScanning" = 262144
+    }
+
+    if($allow){
+        $allowBit = $bitmap[$role]
+        $denyBit = 0
+    } else {
+        $allowBit = 0
+        $denyBit = $bitmap[$role]
+    }
+
+    $uri = "$($ado_org)/_apis/AccessControlEntries/2e9eb7ed-3c0a-47d4-87c1-0ffdd275fd87?api-version=7.1-preview.1"
+    Write-Verbose "Setting Repository Security: $uri" -Verbose
+
+    $payload = @{
+            "token" = "repoV2/$($project_id)/$($repo_id)/"
+            merge = $true
+            accessControlEntries = @(
+                @{
+                    "descriptor" = $descriptor
+                    "allow" = $allowBit
+                    "deny" = $denyBit
+                }
+            )
+     } | ConvertTo-Json -Compress -Depth 10
+
+     Write-Verbose ("Payload for Repository Security: {0}" -f ($payload -replace $service_principal_secret, "*"))
+
+    $payload | Out-File -FilePath "payload.json" -Encoding ascii -Force
+
+    $out = az rest --uri $uri --method POST --resource "499b84ac-1321-427f-aa17-267ca6975798" --output json --body "@payload.json"
+    # Remove-Item -Path "payload.json" -Force
+    $out = $out | ConvertFrom-Json -Depth 10
+    return $out
+}
