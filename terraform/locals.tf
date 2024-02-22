8c1762a5a9e9536d29a66d6d276e688c24b978be
Liam Smith <liam.smith@microsoft.com>
2024-02-22 22:37:32 +0000

    Go

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -9,7 +9,7 @@ locals {
     },
     {
       for principal_id in {
-        data.azurerm_client_config.current.object_id = data.azurerm_client_config.current.object_id
+        "${data.azurerm_client_config.current.object_id}" = data.azurerm_client_config.current.object_id
       } : principal_id.key => principal_id.value
       if var.init
     }
