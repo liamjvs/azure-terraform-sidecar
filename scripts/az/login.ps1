e95d8cbed5e12c531090d01d3dc02de63bcc2aff
Liam Smith <liam.smith@microsoft.com>
2024-02-23 00:16:27 +0000

    Go

--- scripts/az/login.ps1
+++ scripts/az/login.ps1
@@ -6,7 +6,7 @@ if($managed_identity) {
     Write-Verbose "Using Managed Identity" -Verbose
     az login --identity
     $env:ARM_TENANT_ID = (az account show --query tenantId -o tsv)
-    $env:ARM_USE_MSI = "true"
+    $env:ARM_USE_MSI = true
 } elseif ($env:ARM_CLIENT_ID -and $env:ARM_CLIENT_SECRET -and $env:ARM_TENANT_ID) {
     # The AzureCLI task performs a `az account clear` at the end. We want a persistent login across tasks.
     Write-Verbose "Using Service Principal" -Verbose
