9eb5f7153f0eb8fa093514e40900a689db6f9b6c
Liam Smith <liam.smith@microsoft.com>
2024-02-26 22:03:02 +0000

    Go

--- terraform/modules/virtual_network/main.tf
+++ terraform/modules/virtual_network/main.tf
@@ -9,6 +9,6 @@ resource "azurerm_subnet" "this" {
   for_each = var.subnets
   name                 = each.key
   resource_group_name  = var.resource_group_name
-  virtual_network_name = azurerm_virtual_network.vnet.this
+  virtual_network_name = azurerm_virtual_network.this.name
   address_prefixes     = each.value.address_prefixes
 }
