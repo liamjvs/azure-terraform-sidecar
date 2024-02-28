fbc57469c66a827f9c2fbe3e586a8bb231d10dd6
Liam Smith <liam.smith@microsoft.com>
2024-02-28 19:25:50 +0000

    Go

--- terraform/modules/storage_account/output.tf
+++ terraform/modules/storage_account/output.tf
@@ -2,12 +2,8 @@ output "azurerm_storage_account" {
   value = azurerm_storage_account.this
 }
 
-output "azurerm_storage_containers" {
+output "azurerm_storage_container" {
   value = azurerm_storage_container.this
-}
-
-output "azurerm_private_endpoint" {
-  value = azurerm_private_endpoint.this
 }
 
 output "azurerm_role_assignment" {
